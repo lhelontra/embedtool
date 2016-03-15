48fa5145bd148a0b612a18460d3304a94a0f8c1e
Leonardo Lontra <leonardo@debianPc>
2016-03-15 18:16:02 -0300

    adicionado suporte a orange pi

--- targets/opi/afterCopyingData.sh
+++ targets/opi/afterCopyingData.sh
@@ -6,9 +6,4 @@
 # rootfs mountpoint: "$BUILDIMAGE_MOUNTPOINT"/
 #
 
-# habilita o resize
-touch "$BUILDIMAGE_MOUNTPOINT"/root/.need_resize &>/dev/null
-# remove o arquivo do serial
-rm "$BUILDIMAGE_MOUNTPOINT"/root/.serial.txt &>/dev/null
-
 return 0
