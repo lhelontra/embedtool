3622d1d679234739607a1ee7e367abb485fe00f3
lhelontra <lhe.lontra@gmail.com>
2017-10-11 20:37:27 -0300

    added generate debian package script

---
+++ gendeb.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+TEMPDIR=".tmp"
+mkdir -p ${TEMPDIR}/DEBIAN/
+mkdir -p ${TEMPDIR}/usr/local/sbin/
+mkdir -p ${TEMPDIR}/usr/local/share/embedtool/
+
+cp embedtool.sh ${TEMPDIR}/usr/local/sbin/embedtool
+chmod +x ${TEMPDIR}/usr/local/sbin/embedtool
+cp -R targets/ ${TEMPDIR}/usr/local/share/embedtool/
+
+sizeof=$(du -b ${TEMPDIR}/usr/ | tail -n1 | awk '{ print $1 }')
+version="0.1.1"
+arch="all"
+
+cat <<-EOF > ${TEMPDIR}/DEBIAN/control
+Package: embedtool
+Priority: extra
+Section: utils
+Installed-Size: $sizeof
+Maintainer: lhe.lontra@gmail.com
+Architecture: $arch
+Version: $version
+Depends: fdisk (>= 2.29.2-5+b1), mount (>= 2.25.2-6), rsync (>=3.1.1-3), kpartx (>= 0.6.4-5), qemu-user-static (>= 1:2.8+dfsg-6+deb9u2)
+Description: Script to help customization firmwares, uses the image of the environment / directory to crosscompiler. The premise is to use an image or directory with the system to make the crosscompiler / customization operations, and finally generate the desired image.
+EOF
+
+dpkg -b ${TEMPDIR} embedtool_${version}_${arch}.deb
+rm -rf ${TEMPDIR}
