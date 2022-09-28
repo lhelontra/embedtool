0582bc1a411a78d86385977f2a98d88ec1a7e4b3
root <root@debianPc>
2022-09-28 16:30:45 -0300

    small changes

--- gendeb.sh
+++ gendeb.sh
@@ -10,7 +10,7 @@ chmod +x ${TEMPDIR}/usr/local/sbin/embedtool
 cp -R targets/ ${TEMPDIR}/usr/local/share/embedtool/
 
 sizeof=$(du -b ${TEMPDIR}/usr/ | tail -n1 | awk '{ print $1 }')
-version="0.1.3"
+version="0.1.4"
 arch="all"
 
 cat <<-EOF > ${TEMPDIR}/DEBIAN/control
@@ -21,7 +21,7 @@ Installed-Size: $sizeof
 Maintainer: lhe.lontra@gmail.com
 Architecture: $arch
 Version: $version
-Depends: mount (>= 2.25.2-6), util-linux (>=2.25.2-6), rsync (>=3.1.1-3), kpartx (>= 0.6.4-5), qemu-user-static (>= 1:2.1)
+Depends: mount (>= 2.25.2-6), util-linux (>=2.25.2-6), rsync (>=3.1.1-3), kpartx (>= 0.6.4-5), qemu-user-static (>= 1:2.1), e2fsprogs (<= 1.45.5)
 Description: Script to help customization firmwares, uses the image of the environment / directory to crosscompiler. The premise is to use an image or directory with the system to make the crosscompiler / customization operations, and finally generate the desired image.
 EOF
 
